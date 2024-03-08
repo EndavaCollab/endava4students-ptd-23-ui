3753d445affeeae6d6af14d9587bf2693a58bb45
andserban <andreealavinia.serban@endava.com>
2024-03-08 12:29:58 +0200

    Add bg image for authentication container.

--- travel-journal-ui/src/context/AuthContext.jsx
+++ travel-journal-ui/src/context/AuthContext.jsx
@@ -3,7 +3,7 @@ import { createContext, useState } from "react";
 export const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
 
   return (
     <AuthContext.Provider value={{ isAuthenticated, setIsAuthenticated }}>
