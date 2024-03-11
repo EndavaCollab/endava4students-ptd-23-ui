e9a43a4203c4aca711c20c502af62f407f3a8c5a
monica <monica.iliescu99@gmail.com>
2024-03-11 20:38:02 +0200

    add breadcrumbs

--- travel-journal-ui/src/components/Home/Home.jsx
+++ travel-journal-ui/src/components/Home/Home.jsx
@@ -1,7 +1,12 @@
+import React from "react";
+import Breadcrumbs from "../Breadcrumbs/Breadcrumbs";
+
 const Home = () => {
   return (
     <div>
       <h1>Home Page</h1>
+        <Breadcrumbs />
+
     </div>
   );
 };
